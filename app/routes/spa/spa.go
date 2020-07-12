ae1db1a6a5931f1be8a449ebb8e0fa18a092e416
Erwin <espaldd@icloud.com>
2020-07-12 14:27:39 +0200

    Use the internal page

--- app/routes/spa/spa.go
+++ app/routes/spa/spa.go
@@ -1,8 +1,8 @@
 package spa
 
 import (
-	page "github.com/espal-digital-development/espal-core/app/modules/core/app/pages/spa"
 	"github.com/espal-digital-development/espal-core/routing/router/contexts"
+	page "github.com/espal-digital-development/espal-module-core/app/pages/spa"
 )
 
 // Route processor.
